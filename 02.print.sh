9eb8447255f6ffb63b16d10397264dd756404a7b
Sravan <sravank97@gmail.com>
2022-01-30 16:47:13 +0530

    to print colours

--- 02.print.sh
+++ 02.print.sh
@@ -21,3 +21,7 @@ echo Hello universe
 echo -e "\e[31mRED\e[32mGREEN\e[33mYELLOW\e[34mBLUE\e[35mMAGENTA\e[36mCYAN\e[0m"
 
 echo -e "\e[1;31mRED\e[1;32mGREEN\e[1;33mYELLOW\e[1;34mBLUE\e[1;35mMAGENTA\e[1;36mCYAN\e[0m"
+
+
+echo -e "line1\nline2"
+echo -e "word1\tword2"
