812e5bd73b9d96c1ac1fb198837983a59154d183
Sravan <sravank97@gmail.com>
2022-01-31 19:32:33 +0530

    roboshop project structure initiation

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,4 +1,6 @@
 echo "Installing frontend"
-yum install nginx -y >/tmp/roboshop.log
+yum install nginx -y >>/tmp/roboshop.log
 
+echo "Download Frontend Content"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>/tmp/roboshop.log
 
