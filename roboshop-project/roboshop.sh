069c2ff97143a2e16611e41d02897bcfe810068e
Sravan <sravank97@gmail.com>
2022-01-31 18:51:42 +0530

    roboshop project structure initiation

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-bash components/$1.sh
-
+if [ -f components/$1.sh ]; then
+  bash components/$1.sh
+else
+  echo "wrong Input"
+  echo "Available Inputs - frontent|mongodb|catalogue||redis|user|cart|mysql|rabbitmq|payment|dispatch|shippingh"
+fi
