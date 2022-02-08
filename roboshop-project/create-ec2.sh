51f2ef7c5bd1be49bf57f2fb9ff23d47a4a0b725
Sravan <sravank97@gmail.com>
2022-02-08 19:40:08 +0530

    ec2 automation

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+INSTANCE_NAME=$1
+if [ -z "${INSTANCE_NAME}" ]; then
+  echo -e "\e[1;33mInstance Name Argument is needed\e[0m"
+fi
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 if [ -z "${AMI_ID}" ]; then
   echo -e "\e[1;31mUnable to find Image AMI_ID\e[0m"
@@ -7,6 +12,7 @@ else
   echo -e "\e[32mAMI ID = ${AMI_ID}\e[0m"
 fi
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
+
 
 
