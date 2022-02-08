7b0f3aa0b0c1e9e84bbd989fb34f2c884bf67d07
Sravan <sravank97@gmail.com>
2022-02-08 19:19:10 +0530

    ec2 automation

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters "Name-name,values=centos-7-Devops-Practice" --query 'Images[*],[ImageID]' --output text)
+AMI_ID=$(aws ec2 describe-images --filters "Name-name,Values=Centos-7-Devops-Practice" --query 'Images[*],[ImageID]' --output text)
 if [ -z "${AMI_ID}" ]; then
   echo -e "\e[1;31mUnable to find Image AMI_ID\e[0m"
 else
