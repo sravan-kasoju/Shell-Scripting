214556b75398066506b4bcedb809ecfdc5b17b0a
Sravan <sravank97@gmail.com>
2022-02-09 14:13:23 +0530

    ec2 automation

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -16,11 +16,31 @@ fi
 
 PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 if [ -z "${PRIVATE_IP}" ]; then
- aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
+  SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=allow-all --query "SecurityGroups[*].GroupId" --output text)
+if [ -z "${SG_ID}" ]; then
+  echo -e "\e[1;33m Security Group allow-all does not exist"
+  exit
+ fi
+
+ aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-option "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}"
 else
   echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
-  exit
 fi
+
+IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+
+echo '{
+           "Comment" : "CREATE/DELETE/UPSERT a record ",
+            "Changes": [{
+            "Action": "UPSERT",
+                         "ResourceRecordSet": {
+                                        "Name": "DNSNAME",
+                                        "Type": "A",
+                                        "TTL": 300,
+                                    "ResourceRecords": [{ "Value": "IPADDRESS"}]
+}}]
+}' | sed -e "s/DNSNAME/${INSTANCE_NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" >/tmp/record.json
+
 
 
 
