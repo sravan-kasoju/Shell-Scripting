49d39ee04716ec7bd3149aaaebe2deb4c0759031
Sravan <sravank97@gmail.com>
2022-02-09 19:23:12 +0530

    ec2 automation

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -25,6 +25,8 @@ if [ -z "${SG_ID}" ]; then
   exit
  fi
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-option "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}" &>>$LOG
+ echo -e "\e[1m Instance Created\e[0m"
+
 else
   echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
 fi
@@ -45,4 +47,5 @@ echo '{
 
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" --output text | grep roboshop.internal | awk '{print $1}' | awk -F / '{print $3}')
 aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json --output text &>>$LOG
+echo -e "\e[1m DNS Record Created\e[0m"
 
