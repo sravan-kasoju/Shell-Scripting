4fe2921f79b6eb6e8615ff48c70adf56f06b296e
Sravan <sravank97@gmail.com>
2022-02-09 21:43:38 +0530

    functions

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -3,30 +3,30 @@ source components/common.sh
 
 echo "Installing Nginx"
 yum install nginx -y &>>$LOG_FILE
-if [ $? -eq 0 ]; then
- echo -e "\e[1;32m SUCCESS\e[0m"
-else
-  echo -e "\e[1;32m FAILED\e[0m"
- exit
-fi
+STAT $?
 
 echo "Download Frontend Content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+STAT $?
 echo "Clean old content"
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+STAT $?
 
 echo "Extract frontend content"
 cd /tmp
 unzip -o frontent.zip &>>$LOG_FILE
+STAT $?
 
 echo "Copy Extracted content to NGINX path"
 cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
+STAT $?
 
 echo "Copy Nginx Roboshop config"
 cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+STAT $?
 
 echo "Start Nginx service"
 systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx &>>$LOG_FILE
-
+STAT $?
 
