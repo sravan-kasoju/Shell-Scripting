c725cb2664cb9c6ae320dce72beaf839e9aaa6eb
Sravan <sravank97@gmail.com>
2022-02-10 21:47:36 +0530

    mongodb

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -8,22 +8,21 @@ echo "Install MongoDB"
 yum install -y mongodb-org &>>$LOG_FILE
 STAT $?
 
-echo "update MongoDB config file"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
+echo "Update MongoDB Config file"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  &>>$LOG_FILE
 STAT $?
 
-echo "start Database"
-systemctl enable mongod &>>$LOG_FILE
-systemctl start mongod &>>$LOG_FILE
+echo "Start Database"
+systemctl enable mongod  &>>$LOG_FILE && systemctl start mongod &>>$LOG_FILE
 STAT $?
 
 echo "Download Schema"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"  &>>$LOG_FILE
 STAT $?
 
 echo "Extract Schema"
 cd /tmp/
-unzip -o mongodb.zip &>>$LOG_FILE
+unzip -o mongodb.zip  &>>$LOG_FILE
 STAT $?
 
 echo "Load Schema"
