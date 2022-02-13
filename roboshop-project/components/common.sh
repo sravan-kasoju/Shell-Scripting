0da42bfed53ca699999440353769d7b91ce75a74
Sravan <sravank97@gmail.com>
2022-02-13 19:41:23 +0530

    mongodb

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -9,3 +9,60 @@ STAT() {
     exit
   fi
 }
+
+NODEJS() {
+  COMPONENT=$1
+  echo "Setup NodeJs repo"
+  curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>$LOG_FILE
+  STAT $?
+
+  echo "Install NodeJs"
+  yum install nodejs gcc-c++ -y &>>$LOG_FILE
+  STAT $?
+
+  echo "create App user"
+  id roboshop &>>$LOG_FILE
+  if [ $? -ne 0 ]; then
+  useradd roboshop &>>$LOG_FILE
+  fi
+  STAT $?
+
+  echo "Download ${COMPONENT} code"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>$LOG_FILE
+  STAT $?
+
+  echo "Extract ${COMPONENT} code"
+  cd /tmp/ &>>$LOG_FILE
+  unzip -o ${COMPONENT}.zip &>>$LOG_FILE
+  STAT $?
+
+  echo "Clean old user"
+  rm -rf /home/roboshop/${COMPONENT}
+  STAT $?
+
+  echo "copy ${COMPONENT} content"
+  cp -r ${COMPONENT}-main /home/roboshop/${COMPONENT} &>>$LOG_FILE
+  STAT $?
+
+  echo "Install NodeJs Dependencies"
+  cd /home/roboshop/${COMPONENT} &>>$LOG_FILE
+  npm install &>>$LOG_FILE
+  STAT $?
+
+  chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
+
+  echo "update ${COMPONENT} systemD file"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
+  STAT $?
+
+  echo "setup ${COMPONENT} systemD file"
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG_FILE
+  STAT $?
+
+  echo "start ${COMPONENT} service"
+  systemctl daemon-reload &>>$LOG_FILE
+  systemctl enable ${COMPONENT} &>>$LOG_FILE
+  systemctl start ${COMPONENT} &>>$LOG_FILE
+  STAT $?
+
+}
---
+++ roboshop-project/components/user.sh
@@ -0,0 +1,6 @@
+source components/common.sh
+
+NODEJS user
+
+
+
