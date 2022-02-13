8a0104047c4efc441fa44df517ce20628a006cc8
Sravan <sravank97@gmail.com>
2022-02-13 21:19:20 +0530

    create-ec2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -3,6 +3,11 @@
 LOG=/tmp/instance-create.log
 rm -f $LOG
 
+if [ "$1 == "list" ]; then
+ aws ec2 describe-instances --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" --output table
+ exit
+fi
+
 INSTANCE_NAME=$1
 if [ -z "${INSTANCE_NAME}" ]; then
   echo -e "\e[1;33mInstance Name Argument is needed\e[0m"
