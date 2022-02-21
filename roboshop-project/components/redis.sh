924a4a3be03f00a7bc6a700d47818aa971519b35
Sravan <sravank97@gmail.com>
2022-02-21 23:05:28 +0530

    CART

--- roboshop-project/components/redis.sh
+++ roboshop-project/components/redis.sh
@@ -9,18 +9,14 @@ yum install redis -y &>>$LOG_FILE
 STAT $?
 
 echo "Update redis configuration"
-if [ -f /etc/redis.conf ]; then
-sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/redis.conf &>>$LOG_FILE
-elif [ -f /etc/redis/redis.conf ]; then
+if [ -f /etc/redis.conf  ]; then
+  sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/redis.conf &>>$LOG_FILE
+elif [ -f /etc/redis/redis.conf  ]; then
   sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/redis/redis.conf &>>$LOG_FILE
 fi
 STAT $?
 
 echo "Start Redis"
-systemctl enable redis &>>$LOG_FILE
-systemctl restart redis &>>$LOG_FILE
+systemctl enable redis  &>>$LOG_FILE
+systemctl restart redis  &>>$LOG_FILE
 STAT $?
-
-
-
-
