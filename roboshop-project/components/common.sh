d2b93f2ee475bdf4a8e0a1e92a9785119a10cb55
Sravan <sravank97@gmail.com>
2022-02-24 22:58:23 +0530

    mysql

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -1,6 +1,9 @@
 LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
+## BUG about reaching one endpoint , To fix this we are using this command
+rm -f /etc/yum.repos.d/endpoint.repo
+
 STAT() {
   if [ $1 -eq 0 ]; then
     echo -e "\e[1;32m SUCCESS\e[0m"
