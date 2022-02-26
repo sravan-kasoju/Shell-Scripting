00e6fd8a7bf64cb6a879197bf9a36094b64606ee
Sravan <sravank97@gmail.com>
2022-02-26 16:44:49 +0530

    dispatch

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -42,7 +42,7 @@ SYSTEMD_SETUP() {
   chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
 
     echo "update ${COMPONENT} systemD file"
-    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
+    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/RABBITMQ-IP/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
     STAT $?
 
     echo "setup ${COMPONENT} systemD file"
@@ -116,4 +116,23 @@ PYTHON() {
     STAT $?
 
   SYSTEMD_SETUP
+}
+
+GOLANG() {
+  COMPONENT=$1
+
+  echo "Install GoLang"
+  yum install golang -y &>>$LOG_FILE
+
+  APP_USER_SETUP_WITH_APP
+
+  echo "Build GoLang code"
+  cd /home/roboshop/${COMPONENT}
+  go mod intit dispatch &>>$LOG_FILE
+  go get &>>$LOG_FILE
+  go build &>>$LOG_FILE
+  STAT $?
+
+  SYSTEMD_SETUP
+
 }
